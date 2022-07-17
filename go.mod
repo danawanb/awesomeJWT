35c40a977aa0198484d92581c90631a5749b0087
danawanb <danawanbimantoro23@gmail.com>
2022-07-18 01:17:49 +0700

    merge localdb and signup

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module awesomeJWT
 
 go 1.17
 
-require github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+require (
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+)
--- main.go
+++ main.go
@@ -1,156 +1,188 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/golang-jwt/jwt/v4"
-    "log"
-    "net/http"
-    "time"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"time"
 )
+
+var db *sql.DB
+
+func initDB() {
+	var err error
+	// Connect to the postgres db
+	//you might have to change the connection string to add your database credentials
+	db, err = sql.Open("postgres", "dbname=postgres password=mysecretpassword sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
 
 var jwtKey = []byte("my_secret_key")
 
 var users = map[string]string{
-    "user1": "password1",
-    "user2": "password2",
+	"user1": "password1",
+	"user2": "password2",
 }
 
 type Credentials struct {
-    Username string `json:"username"`
-    Password string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 type Claims struct {
-    Username string `json:"username"`
-    jwt.RegisteredClaims
+	Username string `json:"username"`
+	jwt.RegisteredClaims
 }
 
 func Signin(w http.ResponseWriter, r *http.Request) {
-    var creds Credentials
-    err := json.NewDecoder(r.Body).Decode(&creds)
-    if err != nil {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
-    expectedPassword, ok := users[creds.Username]
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	expectedPassword, ok := users[creds.Username]
 
-    if !ok || expectedPassword != creds.Password {
-        w.WriteHeader(http.StatusUnauthorized)
-        return
-    }
+	if !ok || expectedPassword != creds.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 
-    expirationTime := &jwt.NumericDate{time.Now().Add(time.Minute * 15)}
+	expirationTime := &jwt.NumericDate{time.Now().Add(time.Minute * 15)}
 
-    claims := &Claims{
-        Username: creds.Username,
-        RegisteredClaims: jwt.RegisteredClaims{
-            ExpiresAt: expirationTime,
-        },
-    }
+	claims := &Claims{
+		Username: creds.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: expirationTime,
+		},
+	}
 
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    tokenString, err := token.SignedString(jwtKey)
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-    }
-    http.SetCookie(w, &http.Cookie{
-        Name:    "user_token",
-        Value:   tokenString,
-        Expires: expirationTime.Time,
-    })
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:    "user_token",
+		Value:   tokenString,
+		Expires: expirationTime.Time,
+	})
 
 }
 
 func Welcome(w http.ResponseWriter, r *http.Request) {
-    c, err := r.Cookie("user_token")
-    if err != nil {
-        if err == http.ErrNoCookie {
-            w.WriteHeader(http.StatusUnauthorized)
-            return
-        }
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	c, err := r.Cookie("user_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    tknString := c.Value
+	tknString := c.Value
 
-    claims := &Claims{}
+	claims := &Claims{}
 
-    tkn, err := jwt.ParseWithClaims(tknString, claims, func(token *jwt.Token) (interface{}, error) {
-        return jwtKey, nil
-    })
+	tkn, err := jwt.ParseWithClaims(tknString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
 
-    if err != nil {
-        if err == jwt.ErrSignatureInvalid {
-            w.WriteHeader(http.StatusUnauthorized)
-            return
-        }
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    if !tkn.Valid {
-        w.WriteHeader(http.StatusUnauthorized)
-        return
-    }
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 
-    w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
 }
 
 func Refresh(w http.ResponseWriter, r *http.Request) {
-    c, err := r.Cookie("user_token")
+	c, err := r.Cookie("user_token")
 
-    if err != nil {
-        if err == http.ErrNoCookie {
-            w.WriteHeader(http.StatusUnauthorized)
-            return
-        }
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
-    tknString := c.Value
-    claims := &Claims{}
-    tkn, err := jwt.ParseWithClaims(tknString, claims, func(token *jwt.Token) (interface{}, error) {
-        return jwtKey, nil
-    })
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tknString := c.Value
+	claims := &Claims{}
+	tkn, err := jwt.ParseWithClaims(tknString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
 
-    if err != nil {
-        if err == jwt.ErrSignatureInvalid {
-            w.WriteHeader(http.StatusUnauthorized)
-            return
-        }
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
-    if !tkn.Valid {
-        w.WriteHeader(http.StatusUnauthorized)
-        return
-    }
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 
-    expirationTime := &jwt.NumericDate{time.Now().Add(time.Minute * 15)}
-    claims.ExpiresAt = expirationTime
+	expirationTime := &jwt.NumericDate{time.Now().Add(time.Minute * 15)}
+	claims.ExpiresAt = expirationTime
 
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    tokenString, err := token.SignedString(jwtKey)
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-    }
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-    http.SetCookie(w, &http.Cookie{
-        Name:    "user_token",
-        Value:   tokenString,
-        Expires: expirationTime.Time,
-    })
+	http.SetCookie(w, &http.Cookie{
+		Name:    "user_token",
+		Value:   tokenString,
+		Expires: expirationTime.Time,
+	})
 
 }
-func main() {
-    http.HandleFunc("/signin", Signin)
-    http.HandleFunc("/welcome", Welcome)
-    http.HandleFunc("/refresh", Refresh)
 
-    log.Fatal(http.ListenAndServe(":9009", nil))
+func SignUp(w http.ResponseWriter, r *http.Request) {
+	creds := &Credentials{}
+	err := json.NewDecoder(r.Body).Decode(creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
+
+	if _, err = db.Query(`insert into users values ($1, $2)`, creds.Username, string(hashedPassword)); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	initDB()
+	http.HandleFunc("/signup", SignUp)
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/welcome", Welcome)
+	http.HandleFunc("/refresh", Refresh)
+
+	log.Fatal(http.ListenAndServe(":9009", nil))
 
 }
