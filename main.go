2cf2d92fc3361b0c4057b7d6f7d04f5980ba8566
danawanb <danawanbimantoro23@gmail.com>
2022-07-19 00:46:18 +0700

    merge localdb and sign using database

--- main.go
+++ main.go
@@ -5,30 +5,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
+	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 	"time"
 )
 
-var db *sql.DB
-
-func initDB() {
-	var err error
-	// Connect to the postgres db
-	//you might have to change the connection string to add your database credentials
-	db, err = sql.Open("postgres", "dbname=postgres password=mysecretpassword sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
-}
-
 var jwtKey = []byte("my_secret_key")
-
-var users = map[string]string{
-	"user1": "password1",
-	"user2": "password2",
-}
 
 type Credentials struct {
 	Username string `json:"username"`
@@ -47,9 +31,24 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	expectedPassword, ok := users[creds.Username]
+	result := db.QueryRow("select password from users where username=$1", creds.Username)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	storedCreds := &Credentials{}
 
-	if !ok || expectedPassword != creds.Password {
+	err = result.Scan(&storedCreds.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
@@ -74,6 +73,8 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 		Value:   tokenString,
 		Expires: expirationTime.Time,
 	})
+
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", creds.Username)))
 
 }
 
@@ -165,24 +166,74 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 	creds := &Credentials{}
 	err := json.NewDecoder(r.Body).Decode(creds)
 	if err != nil {
+		// If there is something wrong with the request body, return a 400 status
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
 
-	if _, err = db.Query(`insert into users values ($1, $2)`, creds.Username, string(hashedPassword)); err != nil {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(creds.Username)
+	log.Println(string(hashedPassword))
+	SQL := "insert into users(username, password) values ($1, $2)"
+	if _, err = db.Query(SQL, creds.Username, string(hashedPassword)); err != nil {
+		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", creds.Username)))
 }
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "mysecretpassword"
+	dbname   = "postgres"
+)
+
+var db *sql.DB
+
 func main() {
-	initDB()
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	var err interface{}
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
 	http.HandleFunc("/signup", SignUp)
 	http.HandleFunc("/signin", Signin)
 	http.HandleFunc("/welcome", Welcome)
 	http.HandleFunc("/refresh", Refresh)
-
+	http.HandleFunc("/signout", Logout)
 	log.Fatal(http.ListenAndServe(":9009", nil))
 
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	c := &http.Cookie{
+		Name:     "user_token",
+		Value:    "",
+		Path:     "/",
+		MaxAge:   -1,
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, c)
 }
